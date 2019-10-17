0bfae41dcfbc7bea527bfa145962d7a113599181
Bastien Nocera <hadess@hadess.net>
2019-10-17 18:44:34 +0200

    build: Open-code autogen.sh
    Rather than using gnome-common

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="switcheroo-control"
+olddir=`pwd`
+cd "$srcdir"
 
-(test -f $srcdir/configure.ac) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
+aclocal || exit 1
+gtkdocize --copy || exit 1
+autoconf || exit 1
+automake -a -c || exit 1
 
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from the GNOME git"
-	exit 1
-}
+cd "$olddir"
 
-REQUIRED_PKG_CONFIG_VERSION=0.17.1 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+if test -z "$NOCONFIGURE"; then
+	$srcdir/configure --enable-maintainer-mode $*
+fi
